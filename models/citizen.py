e0b81750cea7969e065e3a6a8bed52074f8a9793
Mihai <168391795+kul-work@users.noreply.github.com>
2024-10-15 05:14:07 +0300

    comments to english

--- models/citizen.py
+++ models/citizen.py
@@ -1,32 +1,32 @@
 class Citizen:
     def __init__(self, age, sex, location):
-        # Caracteristici demografice
+        # Demographic characteristics
         self.age = age
         self.sex = sex
         self.location = location
         
-        # Factori economici
+        # Economic factors
         self.income = 0
         self.savings = 0
         self.debt = 0
-        # Evaluare combinată a statutului socioeconomic (0-100)
-        # Poate include factori precum venit, educație, ocupație
+        # Evaluation of socioeconomic status (0-100)
+        # Can include factors such as income, education, occupation
         self.socioeconomic_rating = 0
         
-        # Factori sociali
+        # Social factors
         self.education_level = 0
         self.health = 100
         self.happiness = 50
         self.social_capital = 0
         self.trust_in_institutions = 0
         
-        # Factori de muncă
+        # Work factors
         self.employment_status = "Unemployed"
         self.job_satisfaction = 0
         self.work_life_balance = 0
         
-        # Factori politici și de valori
-        self.political_leaning = 0  # De la -1 (stânga) la 1 (dreapta)
+        # Behavior and lifestyle
+        self.political_leaning = 0  # From -1 (left wing) to 1 (right wing)
         self.civic_engagement = 0
         self.environmental_concern = 0
         
@@ -36,5 +36,5 @@ class Citizen:
         self.leisure_activities = []
         
     def update(self, economy, policies, social_environment):
-        # Logica pentru actualizarea stării cetățeanului
+        # Logic for updating the citizen's state
         pass
